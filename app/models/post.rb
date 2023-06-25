e288a74243e6d138cb4ba6d9203c5d5c30ad2663
Teklu Mezgebo <mezge002@umn.edu>
2023-06-25 18:25:30 -0500

    commit tree 521255f74617eb831f69a282b60e858b3f6e70fb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait
+
+    def clickbait
+        return unless title.present?
+
+        unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess") || title.match?(/\bTop \d+\b/)
+            errors.add(:title, "Not clickbait-y enough")
+        end
+    end
+
 end
